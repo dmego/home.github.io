a45ce09fc370ed36d2dfc90d395f6f4d32de30fc
dmego <zengkai12138@outlook.com>
2017-10-08 00:51:59 +0800

    fix finally

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 // 一言调用
 $.post("https://sslapi.hitokoto.cn/",null,function(e){
-	$('.content').html(e.hitokoto+"<br/> - 「<strong>"+e.from+"</strong>」")
+	$('.content').html(e.hitokoto+"<br/> -「<strong>"+e.from+"</strong>」")
 },'JSON');
