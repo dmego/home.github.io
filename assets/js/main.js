f41b1e34b576c4d392ea2566173ee985406b4f54
dmego <zengkai12138@outlook.com>
2017-10-07 17:50:30 +0800

    fix bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,10 @@
 $.post("https://sslapi.hitokoto.cn/",null,function(e){
 	$('.content').html(e.hitokoto+" —— <strong>"+e.from+"</strong>")
 },'JSON');
+
+// 背景大图加载
+var img = new Image();
+img.src = "http://isecret.vip/images/bg.jpg";
+$(img).load(function(){
+	$('#bg').css("backgroundImage","url("+img.src+")");
+});
