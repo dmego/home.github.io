7ad2ad795b10a3a02b7cea6e2e40e221a2ff51de
Dmego <zengkai12138@outlook.com>
2018-08-20 01:00:50 +0800

    更新个人主页

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,39 @@
 // 一言调用
-$.post("https://sslapi.hitokoto.cn/",null,function(e){
-	$('.content').html(e.hitokoto+"<br/> -「<strong>"+e.from+"</strong>」")
-},'JSON');
+$.post("https://sslapi.hitokoto.cn/", null, function (e) {
+	$('.content').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
+}, 'JSON');
+
+var iUp = (function () {
+	var t = 0,
+		d = 150,
+		clean = function () {
+			t = 0;
+		},
+		up = function (e) {
+			setTimeout(function () {
+				$(e).addClass("up")
+			}, t);
+			t += d;
+		},
+		down = function (e) {
+			$(e).removeClass("up");
+		},
+		toggle = function (e) {
+			setTimeout(function () {
+				$(e).toggleClass("up")
+			}, t);
+			t += d;
+		};
+	return {
+		clean: clean,
+		up: up,
+		down: down,
+		toggle: toggle
+	}
+})();
+
+$(document).ready(function () {
+	$(".iUp").each(function (i, e) {
+		iUp.up(e);
+	});
+});
