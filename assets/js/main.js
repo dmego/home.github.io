2520bf190f0ee6eb85cc564b6b5cd0591af9c498
dmego <dmeago@gmail.com>
2018-10-06 03:18:01 +0800

    fix mobile bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,5 +44,18 @@ $(document).ready(function () {
 	$(".js-avatar")[0].onload = function () {
 		$(".js-avatar").addClass("show");
 	}
+});
 
+$('.btn-mobile-menu__icon').click(function() {
+    if ($('.navigation-wrapper').css('display') == "block") {
+      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+      });
+      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+
+    } else {
+      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    }
+    $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
 });
